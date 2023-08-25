147cd506a98830f288819fac83a514c288bae40b
gedeastu <gede.astu@gmail.com>
2023-08-25 10:56:13 +0800

    up

--- personal/src/app/page.tsx
+++ personal/src/app/page.tsx
@@ -3,10 +3,17 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <>
-    <div className='flex flex-row gap-5'>
-    <Link href={'/about'}>Go to About</Link>
-    <Link href={'/portfolio'}>Go to Portfolio</Link>
-    </div>
+    <article className='bgGradient w-screen h-screen'>
+    <header>
+
+    </header>
+    <main>
+
+    </main>
+    </article>
+    <article>
+
+    </article>
     </>
   )
 }
